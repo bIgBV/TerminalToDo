f885c22f22f20ad62c540e613cb2ec7067be907a
bIgBV <bhargav.v2@gmail.com>
2014-06-30 22:52:36 +0530

    Added formatting to the help output. Still need to fine tune it.

--- TerminalToDo.java
+++ TerminalToDo.java
@@ -42,13 +42,19 @@ class TerminalToDo {
     TerminalToDo term = new TerminalToDo();
     Scanner scn = new Scanner(System.in);
     
-    System.out.println("TerminalToDo: Commandline based to-do application(v 0.0.1)");
+    System.out.println("TerminalToDo: Commandline based to-do application(v 0.1.1)");
     System.out.println("Type 'help' to list all commands");
 
     while(true){
       System.out.print("> ");
       String input = scn.nextLine();
-      if(input.equals("add")){
+      if(input.equals("help")){
+	System.out.println("Welcome to TerminalToDo, the command line to-do list.\nYou have three options:\n");
+	System.out.format("%s10%s","add:", "This command let's you add a new item to the list.\n");
+	System.out.format("%10s%s","show:"," This command shows you all the items present in your list.\n");
+	System.out.format("%10s%s","exit:"," This command exits the application.\n");
+      }
+      else if(input.equals("add")){
 	     System.out.println("New item: ");
 	     String text = scn.nextLine();
 	     term.addItem(text);		
@@ -59,6 +65,9 @@ class TerminalToDo {
       }
       else if(input.equals("exit")){
 	     break;
+      }
+      else{
+	System.out.println("Please enter the right command.");
       }
     }    
   }
